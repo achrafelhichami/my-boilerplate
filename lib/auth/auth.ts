ef76de5629dee55390179ffc9e1a64a17d5cf810
aelhichami <achraf.elhichami@kleegroup.com>
2025-12-07 21:28:48 +0100

    refactoring utils

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,9 +1,9 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { magicLink } from "better-auth/plugins";
-import { prisma } from "./db";
-import { resend } from "./resend";
-import { getMagicLinkEmail } from "./email-templates";
+import { prisma } from "../db";
+import { resend } from "../email/resend";
+import { getMagicLinkEmail } from "../email/email-templates";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
